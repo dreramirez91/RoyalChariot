09b36a353dda4cda2837e0434106c5aa38a3d8c1
Dre <dreramirez91@gmail.com>
2023-06-05 14:46:40 -0500

    First branch commit - created service models, pre-migrations

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.PositiveIntegerField()
+    
+    
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(null=False)
+    
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=200)
+    status = models.CharField(max_length=10, unique=True)
+    vin = models.CharField(max_length=17, unique=True)
+    customer = models.CharField(max_length=200)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointment",
+        on_delete=models.CASCADE
+    )
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# vinfield = charfield we want anyone to be able to bring in their cars not just the ones sold from the inventory
