05052f41bf1c507adf63079f9c233c2dec28c96e
Dre <dreramirez91@gmail.com>
2023-06-06 15:19:53 -0500

    Fixed dropdowns

--- ghi/app/src/ModelForm.js
+++ ghi/app/src/ModelForm.js
@@ -54,6 +54,7 @@ export default function ModelForm() {
             setManufacturer('')
             setModel('')
             setPictureUrl('')
+            setManufacturers('')
         }
     }
     return(
