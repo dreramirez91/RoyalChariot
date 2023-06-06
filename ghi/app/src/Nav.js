46fa4d534354305898164849c0772ba74187f977
Dre <dreramirez91@gmail.com>
2023-06-06 00:52:00 -0500

    Cleaned up nav bar

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -9,18 +9,18 @@ function Nav() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li><NavLink className="nav-link" aria-current="page" to="/manufacturers">Manufacturers</NavLink></li>
-            <li><NavLink className="nav-link" aria-current="page" to="/manufacturers/create">Create a Manufacturer</NavLink></li>
-            <li><NavLink className="nav-link" aria-current="page" to="/models">Models</NavLink></li>
-            <li><NavLink className="nav-link" aria-current="page" to="/models/create">Create a Model</NavLink></li>
-            <li><NavLink className="nav-link" aria-current="page" to="/automobiles">Automobiles</NavLink></li>
-            <li><NavLink className="nav-link" aria-current="page" to="/automobiles/create">Create an Automobile</NavLink></li>
-            <li><NavLink className="nav-link" aria-current="page" to="/technicians">Technicians</NavLink></li>
-            <li><NavLink className="nav-link" aria-current="page" to="/technicians/create">Add a Technician</NavLink></li>
-            <li><NavLink className="nav-link" aria-current="page" to="/appointments/">Appointments</NavLink></li>
-            <li><NavLink className="nav-link" aria-current="page" to="/appointments/create">Create a Service Appointment</NavLink></li>
-            <li><NavLink className="nav-link" aria-current="page" to="/appointments/history">Service History</NavLink></li>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 nav-fill w-100">
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/manufacturers">Manufacturers</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/manufacturers/create">Create a Manufacturer</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/models">Models</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/models/create">Create a Model</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/automobiles">Automobiles</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/automobiles/create">Create an Automobile</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/technicians">Technicians</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/technicians/create">Add a Technician</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/">Appointments</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/create">Create a Service Appointment</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/history">Service History</NavLink></li>
           </ul>
         </div>
       </div>
