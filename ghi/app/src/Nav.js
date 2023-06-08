746dbcc2732617b7472926f82cca9610d3b1d737
Dre <dreramirez91@gmail.com>
2023-06-08 10:46:40 -0500

    About to try bootstrap dropdown

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -8,26 +8,35 @@ function Nav() {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 nav-fill w-100">
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/manufacturers">Manufacturers</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/manufacturers/create">Create a Manufacturer</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/models">Models</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/models/create">Create a Model</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/automobiles">Automobiles</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/automobiles/create">Create an Automobile</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/technicians">Technicians</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/technicians/create">Add a Technician</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/salesperson">Salespeople</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/salesperson/create">Add a Salesperson</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/customer">Customers</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/customer/create">Add a Customer</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/saleslist">Sales</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/newsale/create">Add a sale</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/salespersonhistory">Salesperson History</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/">Appointments</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/create">Create a Service Appointment</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/history">Service History</NavLink></li>
+        <div className="navbar-collapse">
+          <ul className="navbar-nav mb-2 mb-lg-0 nav-fill w-70">
+            <select className="dropdown mr-2" onChange={event => window.location.href = event.target.value}>
+              <option className="nav-item" value="">Inventory</option>
+              <option className="nav-item" value="/manufacturers">Manufacturers</option>
+              <option className="nav-item" value="/manufacturers/create">Create a Manufacturer</option>
+              <option className="nav-item" value="/models">Models</option>
+              <option className="nav-item" value="/models/create">Create a Model</option>
+              <option className="nav-item" value="/automobiles">Automobiles</option>
+              <option className="nav-item" value="/automobiles/create">Create an Automobile</option>
+            </select>
+            <select className="nav-item" onChange={event => window.location.href = event.target.value}>
+              <option value="">Sales</option>
+              <option className="nav-item" value="/salesperson">Salespeople</option>
+              <option className="nav-item" value="/salesperson/create">Add a Salesperson</option>
+              <option className="nav-item" value="/customer">Customers</option>
+              <option className="nav-item" value="/customer/create">Add a Customer</option>
+              <option className="nav-item" value="/saleslist">Sales</option>
+              <option className="nav-item" value="/newsale/create">Add a sale</option>
+              <option className="nav-item" value="/salespersonhistory">Salesperson History</option>
+            </select>
+            <select className="nav-item" onChange={event => window.location.href = event.target.value}>
+              <option className="nav-item" value="">Service</option>
+              <option className="nav-item" value="/appointments">Appointment List</option>
+              <option className="nav-item" value="/appointments/create">Make an Appointment</option>
+              <option className="nav-item" value="/appointments/history">Service History</option>
+              <option className="nav-item" value="/technicians">Technicians List</option>
+              <option className="nav-item" value="/technicians/create">Add a technician</option>
+            </select>
           </ul>
         </div>
       </div>
