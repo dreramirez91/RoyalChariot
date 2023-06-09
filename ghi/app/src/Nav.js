dd654c57e5234b40a54b1c6d867e73ec55eb72cc
Dre <dreramirez91@gmail.com>
2023-06-09 10:29:56 -0500

    Fixed nav

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,12 +1,10 @@
 import { NavLink } from 'react-router-dom';
 import * as Icons from "react-icons/fa"
-import * as Icons from "react-icons/fa"
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar<Icons.FaCar style={{ marginLeft: '2px' }} /></NavLink>
         <NavLink className="navbar-brand" to="/">CarCar<Icons.FaCar style={{ marginLeft: '2px' }} /></NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -55,7 +53,6 @@ function Nav() {
           </ul>
         </div>
       </div>
-    </nav >
     </nav >
   )
 }
