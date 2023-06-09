b670b7f89d4bba11517b13b4a38ff8f360339d72
cougan <cougankidd@yahoo.com>
2023-06-09 09:27:18 -0600

    FINAL

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,11 +1,10 @@
 import { NavLink } from 'react-router-dom';
-import * as Icons from "react-icons/fa"
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar<Icons.FaCar style={{ marginLeft: '2px' }} /></NavLink>
+        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
