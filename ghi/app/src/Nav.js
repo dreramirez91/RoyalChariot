f812d8ce23095451c1679d4256e7185fc162f093
Dre <dreramirez91@gmail.com>
2023-06-16 12:15:33 -0500

    Added car icon back to nav

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,10 +1,11 @@
 import { NavLink } from 'react-router-dom';
+import * as Icons from "react-icons/fa"
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+        <NavLink className="navbar-brand" to="/">CarCar<Icons.FaCar style={{ marginLeft: '2px' }} /></NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
