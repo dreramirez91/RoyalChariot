4116d2b41176957517b29048c08c6eef4b97a04d
Dre <dreramirez91@gmail.com>
2023-06-16 18:17:13 -0500

    Final

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,24 +1,29 @@
 import * as Icons from "react-icons/fa"
 import carVid from "./assets/car.mp4"
+import { NavLink } from "react-router-dom";
 
 function MainPage() {
   return (
     <div className='main'>
-      <div style={{position: 'absolute', top: '0',bottom: '0',width: '100%', height: '100%',backgroundColor: 'rgba(0,0,0,0.6',zIndex: '-1'}} className='overlay'></div>
-      <video src={carVid} autoPlay loop muted style={{position: 'absolute', width: '100%', left: '50%', top: '50%', height: '100%', objectFit: 'cover', transform: 'translate(-50%, -50%)', zIndex: '-2'}} />
+      <div style={{ position: 'absolute', top: '0', bottom: '0', width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.6', zIndex: '-1' }} className='overlay'></div>
+      <video src={carVid} autoPlay loop muted style={{ position: 'absolute', width: '100%', left: '50%', top: '50%', height: '100%', objectFit: 'cover', transform: 'translate(-50%, -50%)', zIndex: '-2' }} />
       <div className='container justify-content-center p-5 m-4'></div>
       <div className='col text-center p-5'>
-      <h1 style={{color: "white"}}>CarCar<Icons.FaCar style={{ fontSize: "30px", marginLeft: '2px' }} /></h1>
-      <div className='text-white text-center m-4 px-5'>
+        <h1 style={{ color: "white" }}>CarCar<Icons.FaCar style={{ fontSize: "30px", marginLeft: '2px' }} /></h1>
+        <div className='text-white text-center m-4 px-5'>
           <div className='text-center'>
             <p className='h3'>
               The premiere solution for automobile dealership management.
             </p>
+          </div>
         </div>
       </div>
-      </div>
+
+      <footer><hr style={{ color: "white" }} />© Copyright CarCar Motors 2023. Registered Office: Miracle Lane, Aspen, Colorado, United States of America. Registered in USA: Number 843912</footer>
     </div>
+
   );
+
 }
 
 
