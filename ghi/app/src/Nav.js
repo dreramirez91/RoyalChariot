06f49cbaee5a6af8ba337004a25c601ef51642af
Dre <dreramirez91@gmail.com>
2023-06-16 18:41:59 -0500

    The Noble Carriage

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar<Icons.FaCar style={{ marginLeft: '2px' }} /></NavLink>
+        <NavLink className="navbar-brand" to="/"><Icons.FaHorseHead style={{ marginLeft: '2px' }} /></NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
