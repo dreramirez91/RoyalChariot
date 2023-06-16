c3973275df722d5c171cf724d3ba8fb0637a4992
Dre <dreramirez91@gmail.com>
2023-06-16 03:52:57 -0500

    Removed autoplay

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -26,7 +26,7 @@ function MainPage() {
       <div>
         <img className="img-fluid max-width-100 height-auto" src={images[currentImageIndex]} />
       </div>
-      <iframe style={{ width: "100%", height: "300" }} scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1105429429&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" />
+      <iframe style={{ width: "100%", height: "300" }} scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1105429429&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" />
         <div style={{ fontSize: '10px', color: '#cccccc', lineBreak: 'anywhere', wordBreak: 'normal', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight: '100'}} >
           <NavLink to="https://soundcloud.com/slanderofficial" title="SLANDER" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>SLANDER</NavLink>
           <NavLink to="https://soundcloud.com/slanderofficial/slander-dylan-matthew-r3hab" title="Love Is Gone ft. Dylan Matthew (R3HAB Remix)" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>Love Is Gone ft. Dylan Matthew R3HAB Remix</NavLink>
